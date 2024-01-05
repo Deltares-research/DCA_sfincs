730e5d9c42e826ee7e756f08bd026e4adad6b6b2
TBovenschen <tycho.bovenschen@deltares.nl>
2024-01-05 13:05:27 +0100

    Add burning in rivers

---
+++ 02_scripts/prepare_river_geometry.py
@@ -0,0 +1,32 @@
+# %%
+from datetime import datetime
+from os.path import basename, join
+
+import geopandas as gpd
+import hydromt
+import matplotlib.pyplot as plt
+import pandas as pd
+import xarray as xr
+from hydromt import DataCatalog
+from hydromt.config import configread
+from hydromt.log import setuplog
+from hydromt_sfincs import SfincsModel
+
+root = r"c:\Git_repos\DCA_sfincs"
+sfincs_root = r"p:\11209905-dca-sfincs-river\01_models\KoldingA"
+input_file = r"p:\11209905-dca-sfincs-river\00_data\geometries\riverlines_kolding_area_cleaned.geojson"
+output_file = r"p:\11209905-dca-sfincs-river\00_data\geometries\riverlines_kolding_area_prepared.geojson"
+
+rivers = gpd.read_file(input_file)
+
+rivers = rivers.rename(columns={"MIDTBREDDE": "rivwth"})
+rivers["rivwth"].where(rivers["rivwth"] != "2.5-12", 7.25, inplace=True)
+rivers["rivwth"].where(rivers["rivwth"] != "Ukendt", 5, inplace=True)
+rivers["rivwth"].where(rivers["rivwth"] != "12-", 15, inplace=True)
+
+rivers["rivdph"] = 1
+rivers["rivwth"] = rivers["rivwth"].astype(float)
+
+rivers.to_file(output_file)
+
+# %%
--- 02_scripts/setup_sfincs_kolding.py
+++ 02_scripts/setup_sfincs_kolding.py
@@ -13,18 +13,34 @@ from hydromt.log import setuplog
 from hydromt_sfincs import SfincsModel
 
 # %%
-root = r'p:\11209905-dca-sfincs-river'
 sfincs_root = r"p:\11209905-dca-sfincs-river\01_models\KoldingA"
 
 logger = setuplog("update", "./hydromt.log", log_level=10)
-yml_file = join(root, "02_scripts", "setup_sfincs_kolding.yml")
+yml_file = join("setup_sfincs_kolding.yml")
 opt = configread(yml_file, abs_path=True)  # read settings from ini file
 kwargs = opt.pop("global", {})
 
-region_fn = join(root, "02_scripts", "input", "sfincs_domain.geojson")
+region_fn = join("input", "sfincs_domain.geojson")
 
 # %%
 mod = SfincsModel(root=sfincs_root, mode="w+", logger=logger, **kwargs)
 mod.build(region={"geom": region_fn}, opt=opt)
 
 # %%
+opt = configread(yml_file, abs_path=True)  # read settings from ini file
+
+
+mod = SfincsModel(root=sfincs_root, mode="r", logger=logger, **kwargs)
+mod.read()
+mod.set_root(sfincs_root + "riv", mode="w+")
+mod.setup_subgrid(**opt["setup_subgrid"])
+mod.write()
+# %%
+opt = configread(yml_file, abs_path=True)  # read settings from ini file
+kwargs = opt.pop("global", {})
+update_model(sfincs_root, sfincs_root + "_riv", opt=opt, kwargs=kwargs)
+
+
+# %%
+
+mod.write()
